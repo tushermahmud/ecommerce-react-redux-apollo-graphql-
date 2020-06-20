c9fa0ab2538f01a73e546b6924d16e956b6d9141
tushermahmud <sazzadmahmud01795355849@gmail.com>
2020-06-20 18:16:17 +0600

    broken our homepage into small components and added styles to our component

---
+++ src/pages/Homepage/HomePageComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import DirectoryComponent from "../../components/directory/DirectoryComponent";
+const HomePage=()=>{
+    return(
+        <div className="homepage">
+            <DirectoryComponent/>
+        </div>
+    )
+}
+export default HomePage;
