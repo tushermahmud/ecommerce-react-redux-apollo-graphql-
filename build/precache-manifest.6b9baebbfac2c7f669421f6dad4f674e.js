dea50c65f980e39f713898279a0d708a0cc44f8b
tushermahmud <sazzadmahmud01795355849@gmail.com>
2020-06-30 22:14:49 +0600

    deploying in heroku

---
+++ build/precache-manifest.6b9baebbfac2c7f669421f6dad4f674e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9af99bd1bb867f0c96ef4686e016aa7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad046e2709cbff753eeb",
+    "url": "/static/css/main.7a78660d.chunk.css"
+  },
+  {
+    "revision": "4707111673320615d17d",
+    "url": "/static/js/2.99738ada.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.99738ada.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad046e2709cbff753eeb",
+    "url": "/static/js/main.3ab833fd.chunk.js"
+  },
+  {
+    "revision": "3cc9d6562f64fc197f20",
+    "url": "/static/js/runtime-main.fc30dc24.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/4.3 crown.svg.ed28accb.svg"
+  }
+]);
